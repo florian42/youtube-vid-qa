8e2ee76c4e03fb96fb282945c70753d69fa9dbf4
Florian Aumeier <hey@flo.fish>
2023-04-29 18:33:52 +0200

    feat: use qa over index chain to circumvent token limitation

--- pages/api/ask.js
+++ pages/api/ask.js
@@ -1,12 +1,13 @@
-import { ChatOpenAI } from "langchain/chat_models";
-import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+import { OpenAI } from "langchain/llms/openai";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { YoutubeTranscript } from "youtube-transcript";
 import unshorter from "unshorter";
 
-const chat = new ChatOpenAI({ temperature: 0 });
-
-const SYSTEM_PROMPT =
-  "You are YouTubeTranscriptChatGPT, the interactive chatbot that can answer questions about YouTube transcripts. You will receive a transcript of a YouTube video. Users can ask questions about the video/transcript, and then you will try to answer them based on the transcript you received. Let's begin!";
+const model = new OpenAI({});
+const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
 
 export default async function handler(request, response) {
   const inputUrl = request.body?.url;
@@ -21,11 +22,15 @@ export default async function handler(request, response) {
   const url = await getUnshortenedUrl(inputUrl);
   const transcript = await fetchTranscript(url);
 
-  const llmResponse = await chat.call([
-    new SystemChatMessage(SYSTEM_PROMPT),
-    new HumanChatMessage(`Transcript of the video: "${transcript}"`),
-    new HumanChatMessage(prompt || "What is the video about?"),
-  ]);
+  const docs = await textSplitter.createDocuments([transcript]);
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever()
+  );
+
+  const llmResponse = await chain.call({ question: prompt, chat_history: [] });
 
   response.status(200).json({ response: llmResponse.text });
 }
